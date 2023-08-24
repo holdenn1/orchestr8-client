4f6a218a006099edfbf66572c065849c9d5f8f7e
holdenn1 <caulfieldd17@gmail.com>
2023-08-24 14:31:42 +0300

    small fixes

--- src/utils/validate/projectFormValidationSchema.ts
+++ src/utils/validate/projectFormValidationSchema.ts
@@ -5,8 +5,5 @@ export default yup.object().shape({
     .string()
     .required('Title is a required field')
     .matches(/^(?!\s)[^\s].*$/, 'Title is a required field'),
-  description: yup
-    .string()
-    .required('Description is a required field')
-    .matches(/^(?!\s)[^\s].*$/, 'Description is a required field'),
+  description: yup.string().required('Description is a required field'),
 });
